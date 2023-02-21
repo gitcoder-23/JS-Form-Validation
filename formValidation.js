77ae34519c48dd248992cdce64b5372bc5496a45
prabhat-cn <prabhat@capitalnumbers.com>
2023-02-21 23:27:29 +0530

    added new validation to git

--- formValidation.js
+++ formValidation.js
@@ -1,18 +1,25 @@
-var form = document.getElementById('employee_form');
-function validFormSubmit(event) {
-    // console.log('form checking');
-    event.preventDefault();
-    var userName = document.getElementById('username').value;
-    console.log('user', userName);
+var formName = document.getElementById('employee_form');
 
-         
-        //   if (userName == '') {
-        //     document.querySelector('#user_err').innerHTML =
-        //       '** Please add username';
-          
-        //     return false;
-        //   }
+// "e" is function argument
+function validFormSubmit(e) {
+  e.preventDefault();
+  var userName = document.getElementById('username').value;
+  var passWord = document.getElementById('password').value;
+  console.log('passWord-->', passWord);
 
-        //   return true;
+  if (userName == '') {
+    document.getElementById('user_err').innerHTML = '* Please add user name';
+    return false;
+  } else {
+    document.getElementById('user_err').innerHTML = '';
+  }
+  if (!passWord) {
+    document.getElementById('pass_err').innerHTML = '* Please add password';
+    return false;
+  } else {
+    document.getElementById('pass_err').innerHTML = '';
+  }
+
+  return true;
 }
-form.addEventListener('submit', validFormSubmit);
+formName.addEventListener('submit', validFormSubmit);
