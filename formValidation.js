a4e6d7583814cb990d421d7dde439b65c20a5c2b
Suman-89 <suman.mukherjee01@gmail.com>
2023-02-22 08:39:56 +0530

    Updated validation

--- formValidation.js
+++ formValidation.js
@@ -4,8 +4,13 @@ var formName = document.getElementById('employee_form');
 function validFormSubmit(e) {
   e.preventDefault();
   var userName = document.getElementById('username').value;
+  console.log('Sumanpass-->', userName);
   var passWord = document.getElementById('password').value;
   console.log('passWord-->', passWord);
+  var confirmPassword = document.getElementById('confirmpass').value;
+  console.log('confirm-password-->', confirmPassword);
+  var mobileNumber = document.getElementById('mobile').value;
+  console.log('mobilenumber-->', mobileNumber);
 
   if (userName == '') {
     document.getElementById('user_err').innerHTML = '* Please add user name';
@@ -19,7 +24,25 @@ function validFormSubmit(e) {
   } else {
     document.getElementById('pass_err').innerHTML = '';
   }
-
+  if (confirmPassword==''){
+    document.getElementById('con_pass_err').innerHTML = '* This field is empty';
+    return false;
+  } else if (confirmPassword!==passWord){
+    document.getElementById('con_pass_err').innerHTML = '* Password Mismatch';
+    return false;
+  } else {
+    document.getElementById('con_pass_err').innerHTML = '';
+  }
+  if (mobileNumber==''){
+    document.getElementById('mobile_err').innerHTML = '* Please add mobile number';
+    return false;
+  } else if (mobileNumber.length !=10){
+    document.getElementById('mobile_err').innerHTML = '*Mobile Number must be 10 digit';
+    return false;
+  } else {
+    document.getElementById('mobile_err').innerHTML = '';
+  }
+  
   return true;
 }
 formName.addEventListener('submit', validFormSubmit);
